97a50f1dfb2ad22311bbfe50dbe5f4ff23349ff4
ellijs <ellijs@hotmail.com>
2022-01-17 16:00:50 -0500

    CR of CRUD

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,5 +1,10 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
+import middy from '@middy/core'
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpErrorHandler from '@middy/http-error-handler';
+import createError from 'http-errors'
 
 // Static!! Perform interactions with DynamoDB Table (Lots of methods, get, patch, put, query and so on)
 const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -10,6 +15,8 @@ async function createAuction(event, context) {
   // 'context' => custome data with middleware, ex) auth, id of user
 
   const { title } = JSON.parse(event.body);
+  // const { title } = event.body;
+  
   const now = new Date();
 
   const auction = {
@@ -21,10 +28,18 @@ async function createAuction(event, context) {
   
   // Use Promise, and await this put method executed and return ID and continue with the result
   // Has to be Capitalized !!
+ try {
   await dynamodb.put({
     TableName: process.env.AUCTIONS_TABLE_NAME, //'AuctionsTable',
     Item: auction, 
   }).promise(); 
+ } catch(error) {
+   console.error(error)
+   throw new createError.InternalServerError(error);
+
+  //  throw new createError.NotFound(error);
+  //  throw new createError.Unauthorized(error);
+ }
 
   return {
     statusCode: 201, // resource created code
@@ -33,9 +48,13 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = createAuction;
-
+export const handler = middy(createAuction)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer()) 
+  .use(httpErrorHandler())
 
 // wen need 'handler' when export !! you can check in yml file that's connected to src/handlers/createAuction/handler
 
 // Lambda only work in 15 mins, we can store static data in here, but we CANNOT store dynamic data in this function.
+
+// middleware : before or after of lambda handler, auth, or validating user token.  => middy 
---
+++ src/handlers/getAuctions.js
@@ -0,0 +1,35 @@
+import AWS from 'aws-sdk';
+import middy from '@middy/core'
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpErrorHandler from '@middy/http-error-handler';
+import createError from 'http-errors'
+
+// Static!! Perform interactions with DynamoDB Table (Lots of methods, get, patch, put, query and so on)
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function getAuctions(event, context) {
+  let auctions;
+
+  try {
+      const result = await dynamodb.scan({
+          TableName: process.env.AUCTIONS_TABLE_NAME
+      }).promise();
+
+      auctions = result.Items
+  } catch (error) {
+      console.error(error);
+      throw new createError.InternalServerError(error)
+  }
+
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(auctions),
+  };
+}
+
+export const handler = middy(getAuctions)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer()) 
+  .use(httpErrorHandler())

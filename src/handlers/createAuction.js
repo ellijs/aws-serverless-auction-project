7f4c27dd08f059799068cc94767b28cf189d3bd0
ellijs <ellijs@hotmail.com>
2022-01-17 21:47:40 -0500

    middleware

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,9 +1,10 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
-import middy from '@middy/core'
-import httpJsonBodyParser from '@middy/http-json-body-parser';
-import httpEventNormalizer from '@middy/http-event-normalizer';
-import httpErrorHandler from '@middy/http-error-handler';
+// import middy from '@middy/core'
+// import httpJsonBodyParser from '@middy/http-json-body-parser';
+// import httpEventNormalizer from '@middy/http-event-normalizer';
+// import httpErrorHandler from '@middy/http-error-handler';
+import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors'
 
 // Static!! Perform interactions with DynamoDB Table (Lots of methods, get, patch, put, query and so on)
@@ -48,10 +49,10 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = middy(createAuction)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer()) 
-  .use(httpErrorHandler())
+export const handler = commonMiddleware(createAuction);
+  // .use(httpJsonBodyParser())
+  // .use(httpEventNormalizer()) 
+  // .use(httpErrorHandler())
 
 // wen need 'handler' when export !! you can check in yml file that's connected to src/handlers/createAuction/handler
 
--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,8 +1,5 @@
 import AWS from 'aws-sdk';
-import middy from '@middy/core'
-import httpJsonBodyParser from '@middy/http-json-body-parser';
-import httpEventNormalizer from '@middy/http-event-normalizer';
-import httpErrorHandler from '@middy/http-error-handler';
+import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors'
 
 // Static!! Perform interactions with DynamoDB Table (Lots of methods, get, patch, put, query and so on)
@@ -29,7 +26,4 @@ async function getAuctions(event, context) {
   };
 }
 
-export const handler = middy(getAuctions)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer()) 
-  .use(httpErrorHandler())
+export const handler = commonMiddleware(getAuctions)
